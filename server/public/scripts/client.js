2757c3443f13a7bfcd9aac9aa82886a9412315c3
Claire Limesand <clairelimesand@gmial.com>
2021-11-14 11:26:11 -0600

    after literally staring at it for a day i finally got the plus function hooked up to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,54 @@
 $(document).ready(onReady);
+let eqationInput = {};
+let addition = false;
+let subtraction = false;
 
 function onReady() {
-    console.log('jquery is loaded!')
-}
+    console.log('jquery working');
+    $('#equalButton').on('click', handleEqualButton);
+    $('#plusButton').on('click', handlePlusButton);
+    $('#minusButton').on('click', handleMinusButton);
+};
+
+function handlePlusButton() {
+    addition = true;
+    subtraction = false;
+};
+
+function handleMinusButton() {
+    addition = false;
+    subtraction = true;
+};
+
+function handleEqualButton() {
+    let inputOne = $('#firstNumber').val();
+    let inputTwo = $('#secondNumber').val()
+    equationInput = {
+        firstNumber: Number(inputOne),
+        secondNumber: Number(inputTwo),
+    }; 
+    if ( addition = true ){
+        equationInput.type = '+'
+    } else if ( subtraction = true ){
+        equationInput.type = '-'
+    }
+    console.log('please tell me this worked', eqationInput);
+    calculateEquation();
+};
+    
+    
+    
+function calculateEquation() {
+    $.ajax ({
+        method: 'POST',
+        url: '/equationInput',
+        data: equationInput,
+    }).then ((response) => {
+        console.log('in POST');
+    }).catch ((error) => {
+        console.log('boo')
+    })
+};
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-// load the express library from node
+const bodyParser = require('body-parser');
+
 const app = express();
-// create our "app" AKA server 
+
 app.use(express.static('./server/public'));
-// tell express where to find our client files 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/equationInput', (req, res) => {
+    console.log('in Post')
+    console.log(req.body);
+    if (req.body.type === '+'){
+        
+    }
+});
+
+
+
 app.listen(5000, function() {
     console.log('this server is running')
 });
-// listens for requests and declares port5000 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json());
-// idk what this stuff does but our group project had it so here it is 
