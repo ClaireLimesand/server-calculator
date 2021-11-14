cd19ceebf199ca367274f10d6f408a6faa54e1f7
Claire Limesand <clairelimesand@gmial.com>
2021-11-14 12:10:54 -0600

    got equation working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,38 +1,62 @@
 $(document).ready(onReady);
-let eqationInput = {};
+let equationInput = {};
 let addition = false;
-let subtraction = false;
+let subtraction = true;
 
 function onReady() {
     console.log('jquery working');
     $('#equalButton').on('click', handleEqualButton);
     $('#plusButton').on('click', handlePlusButton);
     $('#minusButton').on('click', handleMinusButton);
+    $('#multiplyButton').on('click', handleMultiplyButton);
+    $('#divideButton').on('click', handleDivideButton);
 };
 
 function handlePlusButton() {
     addition = true;
     subtraction = false;
+    multiplication = false;
+    division = false;
 };
 
 function handleMinusButton() {
     addition = false;
     subtraction = true;
+    multiplication = false;
+    division = false;
+};
+
+function handleMultiplyButton() {
+    addition = false;
+    subtraction = false;
+    multiplication = true;
+    division = false;
+};
+
+function handleDivideButton() {
+    addition = false;
+    subtraction = false;
+    multiplication = false;
+    division = true;
 };
 
 function handleEqualButton() {
     let inputOne = $('#firstNumber').val();
     let inputTwo = $('#secondNumber').val()
     equationInput = {
-        firstNumber: Number(inputOne),
-        secondNumber: Number(inputTwo),
+        firstNumber: inputOne,
+        secondNumber: inputTwo,
     }; 
-    if ( addition = true ){
+    if ( addition === true ){
         equationInput.type = '+'
-    } else if ( subtraction = true ){
+    } else if ( subtraction === true ){
         equationInput.type = '-'
+    } else if ( multiplication === true ){
+        equationInput.type = '*'
+    } else if ( division === true ){
+        equationInput.type = '/'
     }
-    console.log('please tell me this worked', eqationInput);
+    console.log('please tell me this worked', equationInput);
     calculateEquation();
 };
     
--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+let history = [];
 
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+// I have truly no idea what the bodyParser thing is doing. It was in our group project so I included
+// it here but I'm not sure what it does
 
 app.post('/equationInput', (req, res) => {
     console.log('in Post')
-    console.log(req.body);
-    if (req.body.type === '+'){
-        
-    }
+    calculate(req.body);
 });
 
-
+function calculate(object){
+    let answer = 0;
+    if (object.type === '+'){
+        answer = Number(object.firstNumber) + Number(object.secondNumber)
+    } else if (object.type === '-'){
+        answer = Number(object.firstNumber) - Number(object.secondNumber)
+    } else if (object.type === '*'){
+        answer = Number(object.firstNumber) * Number(object.secondNumber)
+    } else if (object.type === '/'){
+        answer = Number(object.firstNumber) / Number(object.secondNumber)
+    }
+    console.log('the answer is:', answer)
+    return answer;
+};
 
 app.listen(5000, function() {
     console.log('this server is running')
 });
+
+// history.push(`${object.firstNumber} + ${object.secondNumber} = ${answer}`)
