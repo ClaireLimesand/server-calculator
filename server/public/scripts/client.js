016bda5a7b87d521a5454725886a0ea84b2ed4b6
Claire Limesand <clairelimesand@gmial.com>
2021-11-14 19:50:36 -0600

    tried countless things. stared at it for hours. still doesnt work right

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,52 +1,64 @@
 $(document).ready(onReady);
+
+// declare global variables
 let equationInput = {};
-let addition = false;
-let subtraction = true;
+
+let addition;
+let subtraction;
+let multiplication;
+let division;
 
 function onReady() {
     console.log('jquery working');
+    renderHistory();
     $('#equalButton').on('click', handleEqualButton);
     $('#plusButton').on('click', handlePlusButton);
     $('#minusButton').on('click', handleMinusButton);
     $('#multiplyButton').on('click', handleMultiplyButton);
     $('#divideButton').on('click', handleDivideButton);
-};
+    $('#clearButton').on('click', handleClearButton);
+}
 
+// these functions check what kind of equation it is, only
+// the correct variable will be true
 function handlePlusButton() {
     addition = true;
     subtraction = false;
     multiplication = false;
     division = false;
-};
+}
 
 function handleMinusButton() {
     addition = false;
     subtraction = true;
     multiplication = false;
     division = false;
-};
+}
 
 function handleMultiplyButton() {
     addition = false;
     subtraction = false;
     multiplication = true;
     division = false;
-};
+}
 
 function handleDivideButton() {
     addition = false;
     subtraction = false;
     multiplication = false;
     division = true;
-};
+}
 
 function handleEqualButton() {
+    // takes the user inputs and put them into an object
     let inputOne = $('#firstNumber').val();
     let inputTwo = $('#secondNumber').val()
     equationInput = {
         firstNumber: inputOne,
         secondNumber: inputTwo,
     }; 
+    // checks which equation type is true and puts that into 
+    // equationInput as well
     if ( addition === true ){
         equationInput.type = '+'
     } else if ( subtraction === true ){
@@ -56,23 +68,55 @@ function handleEqualButton() {
     } else if ( division === true ){
         equationInput.type = '/'
     }
-    console.log('please tell me this worked', equationInput);
+    console.log('Equation input:', equationInput);
+    // run calculateEquation function
     calculateEquation();
-};
-    
-    
-    
+}
+
 function calculateEquation() {
+    // clear input fields
+    $('#firstNumber').val('');
+    $('#secondNumber').val('');
+    // make a post request sending my equationInput
     $.ajax ({
         method: 'POST',
         url: '/equationInput',
         data: equationInput,
     }).then ((response) => {
-        console.log('in POST');
+        console.log('response', response)
     }).catch ((error) => {
-        console.log('boo')
+        console.log('error', error)
     })
-};
+}
 
+function renderHistory() {
+    $.ajax({
+        method: 'GET',
+        url: '/equation'
+    }).then((response) => {
+        console.log('response', response);
+        // clear history
+        $('#history').empty();
+        // loop through the response
+        for (let number of response) {
+            // append the response to DOM
+            $('#history').append(`
+                <li>${number.firstNumber} ${number.type} ${number.secondNumber} = ${number.total}</li>
+            `);
+        }  
+         // answer area and append my response.total to the DOM
+        $('#finalAnswer').empty();
+        $('#finalAnswer').append(`
+        ${response[response.length - 1].total}
+        `); 
+        }).catch((error) => {
+        console.log('error', error);
+    });
+}
 
-
+// clear my input values and history
+function handleClearButton() {
+    $('#firstNumber').val('');
+    $('#secondNumber').val('');
+    $('#history').empty();
+}
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-let history = [];
+let equationData = [];
 
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,28 +10,35 @@ app.use(bodyParser.json());
 // I have truly no idea what the bodyParser thing is doing. It was in our group project so I included
 // it here but I'm not sure what it does
 
-app.post('/equationInput', (req, res) => {
-    console.log('in Post')
-    calculate(req.body);
+// this get function sends back equationData
+app.get('/equation', (req, res) => {
+    console.log('in GET')
+    res.send(equationData);
 });
 
+// this take equationInput data and runs the calculate function
+app.post('/equationInput', (req, res) => {
+    console.log('in POST', req.body);
+    calculate(req.body);
+    res.sendStatus(201);
+});
+
+// this checks the object type and equates based on that 
 function calculate(object){
-    let answer = 0;
     if (object.type === '+'){
-        answer = Number(object.firstNumber) + Number(object.secondNumber)
+        object.total = Number(object.firstNumber) + Number(object.secondNumber);
     } else if (object.type === '-'){
-        answer = Number(object.firstNumber) - Number(object.secondNumber)
+        object.total = Number(object.firstNumber) - Number(object.secondNumber);
     } else if (object.type === '*'){
-        answer = Number(object.firstNumber) * Number(object.secondNumber)
+        object.total = Number(object.firstNumber) * Number(object.secondNumber);
     } else if (object.type === '/'){
-        answer = Number(object.firstNumber) / Number(object.secondNumber)
+        object.total = Number(object.firstNumber) / Number(object.secondNumber);
     }
-    console.log('the answer is:', answer)
-    return answer;
-};
+    // i push this information to my equation data array 
+    equationData.push(object);
+    console.log(equationData)
+}
 
 app.listen(5000, function() {
     console.log('this server is running')
-});
-
-// history.push(`${object.firstNumber} + ${object.secondNumber} = ${answer}`)
+})
